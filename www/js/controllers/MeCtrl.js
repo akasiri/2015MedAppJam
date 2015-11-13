ed610b7b0f0d11abc5a25d2523737450171f98d1
yangjiao2 <yjiao7@uci.edu>
2015-11-13 15:33:11 -0800

    will work on the scroll and resize the list frame

--- www/js/controllers/MeCtrl.js
+++ www/js/controllers/MeCtrl.js
@@ -9,6 +9,48 @@ main.controller('MeCtrl', ['$scope','$state', 'userFactory', function($scope, $s
         $state.go('edit_profile', {}, {reload: true});
     };
 
+  //$http.get("http://jsonplaceholder.typicode.com/posts/").then(function(result){
+  //  // console.log(result.data);
+  //  $scope.posts = result.data;
+  //});
+    $scope.posts = [
+      {
+        title: 'lol',
+        date: '28 Nov'
+      },
+      {
+        title: 'pop',
+        date: '30 Oct'
+      },
+      {
+        title: 'hoho',
+        date: '1 Sep'
+      },
+      {
+        title: 'yolo',
+        date: '12 Sep'
+      }
+
+    ];
+
+
+  $scope.shouldShowDelete = false;
+  $scope.shouldShowReorder = false;
+  $scope.listCanSwipe = true
+
+
+  $scope.noMoreItemsAvailable = false;
+
+  $scope.loadMore = function() {
+    $scope.items.push({ id: $scope.items.length});
+
+    if ( $scope.items.length == posts.length ) {
+      $scope.noMoreItemsAvailable = true;
+    }
+    $scope.$broadcast('scroll.infiniteScrollComplete');
+  };
+
+
     var refresh = function() {
         //console.log(Parse.User.current());
         userFactory.fetchcurrent();
