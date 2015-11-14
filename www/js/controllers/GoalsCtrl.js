c75d9765fdc841539d8e1029133940a700bfa614
akasiri <akasiri@uci.edu>
2015-11-14 13:29:00 -0800

    lists all goals. can submit goals from the bottom of the list. by sliding left on a goal, the user can reveal a remove button that removes the goal.

--- www/js/controllers/GoalsCtrl.js
+++ www/js/controllers/GoalsCtrl.js
@@ -9,12 +9,49 @@ main.controller('GoalsCtrl', ['$scope', '$state', '$ionicModal', '$ionicPopup', 
      * NOTIFICATIONS/ IONIC POP UP
      */
 
+    $scope.goals = [];
+    $scope.newGoal = "";
+
+
+    var checkIfInArray = function(x, list) {
+        var listLen = list.length;
+        for (var i = 0; i < listLen; i++) {
+            if (x == list[i]) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    $scope.remove = function(goal) {
+        $scope.goals.splice($scope.goals.indexOf(goal), 1);
+    };
+
+    $scope.add = function(newGoal) {
+        if (!checkIfInArray(newGoal, $scope.goals) && (newGoal != "")) {
+            $scope.goals.push({text:newGoal,complete:false});
+        }
+        else {
+            var alertPopup = $ionicPopup.alert({
+              title: 'Don\'t eat that!',
+              template: 'Cannot add to the list'
+            });
+            alertPopup.then(function(res) {
+                // pass
+            });
+        }
+    };
+
+    $scope.toggleComplete = function(goal) {
+        goal.complete = !goal.complete;
+    };
+
     $scope.setting = function() {
       console.log("going to setting");
       $state.go('setting');
     };
 
-    $scope.goals = ["washing the car", "walking the dog", "get gold"];
+//    $scope.goals = ["washing the car", "walking the dog", "get gold"];
     //Alert success if added the transaction, then clear the fields so user can send another transaction
     var alertSuccess = function () {
         var alertPopup = $ionicPopup.alert({
